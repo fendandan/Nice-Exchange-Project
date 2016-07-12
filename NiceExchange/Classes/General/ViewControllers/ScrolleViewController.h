0cbd38de37ce0da09b993ba97037427b171aabf2
Lixinbo <1097026020@qq.com>
2016-07-12 21:20:21 +0800

    创建 scrollerView 基类

---
+++ NiceExchange/Classes/General/ViewControllers/ScrolleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScrolleViewController.h
+//  NiceExchange
+//
+//  Created by lanou3g on 16/7/12.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ScrolleViewController : BaseViewController
+
+@end
---
+++ NiceExchange/Classes/General/ViewControllers/ScrolleViewController.m
@@ -0,0 +1,47 @@
+//
+//  ScrolleViewController.m
+//  NiceExchange
+//
+//  Created by lanou3g on 16/7/12.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import "ScrolleViewController.h"
+
+@interface ScrolleViewController ()
+
+@property(nonatomic,strong)UIScrollView *scrollView;
+
+@end
+
+@implementation ScrolleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+//    self.scrollView = [[UIScrollView alloc] initWithFrame:[UIScreen mainScreen]];
+    
+    
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
