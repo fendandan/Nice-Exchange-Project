638f21269120b405fee2eaf1b6a22783c398889a
fendandan <1004372499@qq.com>
2016-07-12 20:32:39 +0800

    first update 一个空的工程 增加了自定义的AppDelegate，新建了BaseVIewController

---
+++ NiceExchange/Classes/General/ViewControllers/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  NiceExchange
+//
+//  Created by lanou3g on 16/7/12.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
