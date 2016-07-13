ee356716e3f2f8bc53931fa922050c8a82b53e2f
myfreee <704297103@qq.com>
2016-07-13 12:57:19 +0800

    重置工程

---
+++ NiceExchange/Classes/General/ViewControllers/BaseOrganViewController.m
@@ -0,0 +1,123 @@
+//
+//  BaseOrganViewController.m
+//  NiceExchange
+//
+//  Created by Spacewalk on 16/7/12.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import "BaseOrganViewController.h"
+
+@interface BaseOrganViewController ()<UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout
+>
+
+
+@property (nonatomic , strong) UICollectionView *collectionview;
+@end
+
+@implementation BaseOrganViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"collectionview";
+    
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
+    
+    self.collectionview = [[UICollectionView alloc]initWithFrame:[UIScreen mainScreen].bounds collectionViewLayout:layout];
+    self.collectionview.dataSource = self;
+    self.collectionview.delegate = self;
+    self.collectionview.backgroundColor = [UIColor redColor];
+    [self.collectionview registerNib:[UINib nibWithNibName:@"BaseOrganCollectionViewCell" bundle:[NSBundle mainBundle]]
+     
+          forCellWithReuseIdentifier:@"baseCell"];
+    [self.view addSubview:self.collectionview];
+    
+}
+
+
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    
+    
+    return 20;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    BaseOrganCollectionViewCell  *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"baseCell" forIndexPath:indexPath];
+    cell.baseImv.image = [UIImage imageNamed:@"zheng_5.jpg"];
+    cell.baseLable.text = @"collectionView";
+    
+    return cell;
+    
+}
+
+#pragma mark - UICollectionViewDelegateFlowLayout
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(self.view.bounds.size.width / 3 - 10, self.view.frame.size.width / 3 - 10);
+}
+
+// 设置每个cell上下左右相距
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsMake(5, 5, 5, 5);
+}
+
+// 设置最小行间距，也就是前一行与后一行的中间最小间隔
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 5;
+}
+
+// 设置最小列间距，也就是左行与右一行的中间最小间隔
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 5;
+}
+
+// 设置section头视图的参考大小，与tableheaderview类似
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section {
+    return CGSizeMake(self.view.frame.size.width, 150);
+}
+
+// 设置section尾视图的参考大小，与tablefooterview类似
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section {
+    return CGSizeMake(self.view.frame.size.width, 40);
+}
+
+// 设置是否允许选中
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (indexPath.item == 1) {
+        return NO;
+    }
+    NSLog(@"%ld",indexPath.item);
+    return YES;
+}
+
+// 设置是否允许取消选中
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldDeselectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    return YES;
+}
+
+// 选中操作
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
