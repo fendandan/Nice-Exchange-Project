422e79990c0077d5e6a18b35ce99231d62166d06
myfreee <704297103@qq.com>
2016-07-13 21:47:16 +0800

    重置 工程代码

---
+++ NiceExchange/Classes/General/ViewControllers/BaseOrganViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseOrganViewController.h
+//  NiceExchange
+//
+//  Created by Spacewalk on 16/7/12.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseOrganViewController : BaseViewController
+
+@end
---
+++ NiceExchange/Classes/Helpers/Request/BaseRequest.h
@@ -0,0 +1,13 @@
+//
+//  BaseRequest.h
+//  NiceExchange
+//
+//  Created by Spacewalk on 16/7/13.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseRequest : NSObject
+
+@end
