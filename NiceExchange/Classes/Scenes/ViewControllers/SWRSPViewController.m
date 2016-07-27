20d7c8ad012a68a5f9b5f9b929d8c28900e3a1cf
fendandan <1004372499@qq.com>
2016-07-27 09:03:32 +0800

    增加重置密码页面

---
+++ NiceExchange/Classes/Scenes/ViewControllers/SWRSPViewController.m
@@ -0,0 +1,37 @@
+//
+//  SWRSPViewController.m
+//  NiceExchange
+//
+//  Created by Spacewalk on 16/7/26.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import "SWRSPViewController.h"
+
+@interface SWRSPViewController ()
+
+@end
+
+@implementation SWRSPViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
