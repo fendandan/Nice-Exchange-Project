87174a52c375157c35d06819f513aa7839670356
Lixinbo <1097026020@qq.com>
2016-07-29 20:15:22 +0800

    添加存草稿功能

---
+++ NiceExchange/Classes/Scenes/Message/ViewControllers/SWDraftTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  SWDraftTableViewCell.h
+//  NiceExchange
+//
+//  Created by Spacewalk on 16/7/29.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define SWDraftTableViewCell_Identifiter @"SWDraftTableViewCell_Identifiter"
+
+@interface SWDraftTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (strong, nonatomic) IBOutlet UILabel *timeLabel;
+
+
+@end
---
+++ NiceExchange/Classes/Scenes/Message/ViewControllers/SWDraftViewController.m
@@ -0,0 +1,114 @@
+//
+//  SWDraftViewController.m
+//  NiceExchange
+//
+//  Created by Spacewalk on 16/7/29.
+//  Copyright © 2016年 Spacewalk. All rights reserved.
+//
+
+#import "SWDraftViewController.h"
+#import "SWDraftTableViewCell.h"
+#import "SWCreationViewController.h"
+#import "DataBaseHandle.h"
+#import "SWCreationViewController.h"
+
+@interface SWDraftViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+
+@property(nonatomic,strong)UITableView *tableView;
+
+@end
+
+@implementation SWDraftViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    [self addTableView];
+ 
+}
+
+
+
+
+
+
+
+- (void)addTableView{
+    
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.height) style:(UITableViewStylePlain)];
+    
+    [self.view addSubview:self.tableView];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self.tableView registerNib:[UINib nibWithNibName:@"SWDraftTableViewCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:SWDraftTableViewCell_Identifiter];
+}
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    SWDraftTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SWDraftTableViewCell_Identifiter forIndexPath:indexPath];
+    
+    
+    cell.titleLabel.text = self.titleStr;
+    
+    cell.timeLabel.text = self.textViewStr;
+    
+    
+    return cell;
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    return 80;
+    
+}
+
+
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    SWCreationViewController *swVC = [SWCreationViewController new];
+    
+    swVC.titleStr = self.titleStr;
+    swVC.labelStr = self.labelStr;
+    swVC.textViewStr = self.textViewStr;
+    swVC.ruleStr = self.ruleStr;
+    
+    [self.navigationController pushViewController:swVC animated:YES];
+    
+}
+
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
